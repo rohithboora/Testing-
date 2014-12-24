0946bf3ad862bf93900ab6c99256bc2689ce15ff
rohithboora <rohithboora@gmail.com>
2014-12-24 14:10:00 +0000

    first push

---
+++ src/test/java/com/saucelabs/SampleSauceTest.java
@@ -0,0 +1,41 @@
+package com.saucelabs;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.URL;
+import static org.junit.Assert.assertEquals;
+
+public class SampleSauceTest {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        // Choose the browser, version, and platform to test
+        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+        capabilities.setCapability("version", "5");
+        capabilities.setCapability("platform", Platform.XP);
+        // Create the connection to Sauce Labs to run the tests
+        this.driver = new RemoteWebDriver(
+                new URL("http://rohithboora:f55a6595-3f95-4da3-a0f2-c330059dcd50@ondemand.saucelabs.com:80/wd/hub"),
+                capabilities);
+    }
+
+    @Test
+    public void webDriver() throws Exception {
+        // Make the browser get the page and check its title
+        driver.get("http://www.amazon.com/");
+        assertEquals("Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more", driver.getTitle());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+}
